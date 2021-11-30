b38da6efdf71908a454633e89ab68749bd671d68
Abigail Cisowska <88333541+Amy-CoolDog@users.noreply.github.com>
2021-11-30 09:37:40 +0000

    Add files via upload

---
+++ reg_file/register_32-20333898.vhd
@@ -0,0 +1,23 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+entity register_32 is
+  Port ( D : in std_logic_vector(31 downto 0);
+         load, Clk : in std_logic;
+         Q : out std_logic_vector(31 downto 0)
+       );
+end register_32;
+
+architecture Behavioral of register_32 is
+  begin
+    process(Clk)
+    begin
+      if (rising_edge(Clk)) then
+        if load='1' then
+          Q <= D after 5 ns;
+        end if;
+      end if;
+    end process;
+  end Behavioral;
